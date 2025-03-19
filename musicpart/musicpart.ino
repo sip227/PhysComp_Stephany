19bb9a7939a49bd9da50eabe1378522944b8c479
sip227 <stephany.pena@mail.citytech.cuny.edu>
2025-03-18 23:04:01 -0400

    uploading to github to work on laptop

---
+++ musicpart/musicpart.ino
@@ -0,0 +1,31 @@
+//MIDTERM
+
+//Libraries
+#include "pitches.h"
+
+//PIEZO
+// const int piezoPin1 =;
+// const int piezoPin2 =;
+// const int piezoPin3 =;
+
+//LED 
+// const int  ledPin1 = ;
+// const int  ledPin2 = ;
+// const int  ledPin3 = ;
+// const int  ledPin4 = ;
+// const int  ledPin5 = ;
+
+//POT
+
+//BUTTON
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
